5a715efcfef6a8499613e437d498fdbf116daeae
jeffreywescott <jeffrey@learnersguild.org>
2016-02-22 23:23:25 -0800

    call next middleware after auth callback

--- src/configureAuthRoutes.js
+++ src/configureAuthRoutes.js
@@ -8,11 +8,12 @@ export function configureAuthRoutes(app, authURL, callbackURL) {
 
   app.get(callbackURL,
     passport.authenticate('auth0', {failureRedirect: '/'}),
-    (req, res) => {
+    (req, res, next) => {
       if (!req.user) {
         throw new Error('authentication failed!')
       }
       res.cookie('jwt', req.user.idToken, {secure: process.env.NODE_ENV === 'production', httpOnly: true})
+      next()
       res.redirect('/')
     }
   )

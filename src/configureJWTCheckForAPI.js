f5ca2c5f8d533c60c95a2105e04bd837851afb26
jeffreywescott <jeffrey@learnersguild.org>
2016-02-26 11:48:19 -0800

    use positive paths rather than ignore paths

--- src/configureJWTCheckForAPI.js
+++ src/configureJWTCheckForAPI.js
@@ -1,12 +1,10 @@
 import jwt from 'express-jwt'
 
-export function configureJWTCheckForAPI(app, clientID, clientSecret, ignorePaths = []) {
+export function configureJWTCheckForAPI(app, clientID, clientSecret, apiPaths = []) {
   // Ensure that the caller has a valid JWT token to access this API.
   const jwtCheck = jwt({
     audience: clientID,
     secret: new Buffer(clientSecret, 'base64'),
-  }).unless({
-    path: ignorePaths,
   })
-  app.use(jwtCheck)
+  apiPaths.forEach(path => app.use(path, jwtCheck))
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import {configureAuthRoutes} from './configureAuthRoutes'
 import {configureJWTCheckForAPI} from './configureJWTCheckForAPI'
 import {configureRequestUserFromJWT} from './configureRequestUserFromJWT'
 
-export default function configureAuth0(app, {domain, clientID, clientSecret, authURL, callbackURL, jwtIgnorePaths}) {
+export default function configureAuth0(app, {domain, clientID, clientSecret, authURL, callbackURL, jwtAPIPaths}) {
   if (!domain || !clientID || !clientSecret || !authURL || !callbackURL) {
     throw new Error('Required options: domain, clientID, clientSecret, authURL, callbackURL')
   }
 
   // all API calls need a proper JWT
-  configureJWTCheckForAPI(app, clientID, clientSecret, jwtIgnorePaths)
+  configureJWTCheckForAPI(app, clientID, clientSecret, jwtAPIPaths)
   // if a JWT cookie is found, grab the user from Auth0 and attach it to the request for
   // server-side rendering
   configureRequestUserFromJWT(app, domain)
